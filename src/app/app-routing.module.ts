6b54a0ed78d9c52dbd4076a47f77c6ceb9d28c80
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2021-10-22 20:39:35 -0300

    se crea clase modulo de rutas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+
+@NgModule({
+
+})
+//se crea la clase para redireccionar las paginas para que se mas facil de mantener
+export class AppRoutingModule{
+
+}
