4e279b685befc6533e66f866b0ee539aff10e1de
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2021-10-22 21:32:43 -0300

    se configura el modulo de routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,30 @@
-import { NgModule } from "@angular/core";
+import { NgModule, Component } from '@angular/core';
 
+//routerModule dice si son rutas principales o fijas a angular
+import {Routes,RouterModule} from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+
+//constante de tipo Routes
+//arreglo de rutas a donde va redirigir
+const app_routes: Routes = [
+    //ruta vacia
+    {path: '',component: PortafolioComponent },
+    {path:'about',component:AboutComponent},
+    {path: 'item',component:ItemComponent},
+    //ruta que no este aca / exepcion 
+    //y redireccion a portafolio 1ra ruta
+    //pathMatch para que sea cualquier ruta
+    {path: '**', pathMatch: 'full',redirectTo: ''}
+
+];
+//importo un arreglo de rutas , el que tengo arriba
 @NgModule({
+    imports: [
+        RouterModule.forRoot( app_routes)
+    ]
 
 })
 //se crea la clase para redireccionar las paginas para que se mas facil de mantener
