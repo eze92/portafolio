f7618c7f437c85dccc60b991129681b3879eaef3
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2021-11-05 20:34:24 -0300

    se agrega nuevas propiedades para tomar el json

--- src/app/services/infopagina.service.ts
+++ src/app/services/infopagina.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfopaginaService {
 
-  constructor() { 
-    console.log('servicio de infoPagina listo');
+  info: any = {};
+  cargada = false;
+
+  constructor(private htpp: HttpClient) { 
+   // console.log('servicio de infoPagina listo');
+//ruta del json
+    this.htpp.get('assets/data/data-pagina.json')
+    //suscribe recibe una respuesta 
+    .subscribe(resp =>{
+    
+      this.cargada = true;
+      this.info = resp;
+      console.log(resp);
+    })
   }
 }
