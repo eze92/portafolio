be15c162b99a2252023bafb1ad94f8736c64aefb
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2021-11-16 21:50:34 -0300

    se afrega el nFor

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from '../../services/infoPagina.service';
 
 @Component({
   selector: 'app-about',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  //inyecto el infoService para ser utilizado en el html
+  constructor(public infoService:InfoPaginaService) { }
 
   ngOnInit(): void {
   }
