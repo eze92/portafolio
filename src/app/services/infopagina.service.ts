6aeeeffa5810abd782fa8954a27db0bc566d45f6
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2021-11-16 21:08:41 -0300

    se modifica infoPagina.service agregando nuevo metodos y modificando el constructor

--- src/app/services/infopagina.service.ts
+++ src/app/services/infopagina.service.ts
@@ -10,16 +10,34 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor(private htpp: HttpClient) { 
-   // console.log('servicio de infoPagina listo');
-//ruta del json
+
+    this.cargarInfo();
+    this.cargarEquipo();
+  
+    }
+    private cargarInfo(){
+       // console.log('servicio de infoPagina listo');
+      //ruta del json
     this.htpp.get('assets/data/data-pagina.json')
     //suscribe recibe una respuesta 
     .subscribe((resp: InfoPagina) =>{
     
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
-    })
+  //    console.log(resp);
+    }); 
   }
+    private cargarEquipo(){
+      this.htpp.get('https://angular-html-8b8c1-default-rtdb.firebaseio.com/equipo.json')
+    //suscribe recibe una respuesta 
+    .subscribe( (resp: any ) =>{
+    
+      this.equipo = resp;
+      console.log(resp);
+    }); 
+  }
+  
 }
