0f081207b766649254846bb89f436ef72cac67db
Ezequiel Ledesma <ezequiel.ledesma026@gmail.com>
2021-12-31 14:13:43 -0300

    se agrego logica de busqueda

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { InfoPaginaService } from '../../services/infoPagina.service';
 
 
@@ -9,12 +10,18 @@ import { InfoPaginaService } from '../../services/infoPagina.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(public _servicio: InfoPaginaService) { }
+  constructor(public _servicio: InfoPaginaService,
+              private router : Router) { }
 
   ngOnInit(): void {
   }
   buscarProducto(termino: string){
-    console.log(termino);
+
+    if (termino.length < 1) {
+      return;
+    }
+    this.router.navigate(['/search',termino]);
+   
   }
 
 }
